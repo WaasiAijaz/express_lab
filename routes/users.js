c0d47a11df4b5470618f4d936cd6b1237b04764e
WaasiAijaz <waasiaijaz786@gmail.com>
2025-11-12 13:47:21 -0500

    Made ID route

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.send('User List');
+});
+router.get('/new', (req, res)=>{
+    res.send('New User Form');
+});
+// router.post('/',(req,res)=>{
+// });
+// router.get('/:id', (req,res)=>{
+//     res.send(`Getting User data: ${req.params.id}`);
+// });
+
+router.route("/:id").get('/:id', (req,res)=>{
+    res.send(`Getting User Data: ${req.params.id}`);
+}).delete((req,res)=>{
+    res.send(`Deleting User with id: ${req.params.id}`);
+}).put((req,res)=>{
+    res.send(`Updating user with id: ${req.params.id}`);
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
-
+const userRouter = require('./routes/users');
 const app = express(); //Calling this function sets up a server
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');''
+app.use('/users', userRouter);
 
 app.get('/', (req, res)=>{
     console.log('Here');
     res.render("index", {user:"Waasi!"});
 });//this function will run when someone goes to the root folder
-app.post('/users', (req, res)=>{
-    res.send('User List');
-});
-app.get('/users/new', (req, res)=>{
-    res.send('New User Form');
-});
 
 app.listen(3030);
